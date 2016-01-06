2a52682fb489c1cd84e6ee32a9d92077675c7eef
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2016-01-06 15:32:49 -0500

    Generate users using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
-	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255}
-	validates :email, :email => true
-	has_secure_password
-	validates :password, presence: true, length: { minimum: 6}
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 end
