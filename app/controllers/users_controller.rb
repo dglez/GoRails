407cb0681c4f8d7956975f7a940f1365fe59887a
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2016-01-06 18:04:21 -0500

    Add user redirection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
 	before_action :user_admin?, only: [:index]
 	def index
-
+		@users = User.all
 	end
 
+	def show
+		@user = User.find params[:id]
+	end
 
 	private
 
 	def user_admin?
-		redirect_to 'root' if !current_user.user_admin
+		redirect_to :root if !current_user.admin
 	end
 end
